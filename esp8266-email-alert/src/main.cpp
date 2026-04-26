603994bdea5fc583a9c33969720708e7c1d567b8
johnkok <johnkokko@hotmail.com>
2026-04-26 09:48:55 +0300

    feat: Add main application entry point

---
+++ esp8266-email-alert/src/main.cpp
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <SPIFFS.h>
+#include <ArduinoJson.h>
+#include "config.h"
+#include "wifi_ap.h"
+#include "gpio_monitor.h"
+#include "email_client.h"
+#include "web_server.h"
+
+void setup() {
+    Serial.begin(115200);
+    delay(1000);
+    
+    Serial.println("\n\nESP8266 Email Alert System Starting...");
+    
+    if (!SPIFFS.begin()) {
+        Serial.println("SPIFFS Mount Failed");
+        return;
+    }
+    Serial.println("SPIFFS Mounted");
+    
+    Config::load();
+    WiFiAP::setup();
+    GPIOMonitor::setup();
+    EmailClient::setup();
+    WebServer::setup();
+    
+    Serial.println("Setup Complete");
+}
+
+void loop() {
+    WiFiAP::handle();
+    WebServer::handle();
+    GPIOMonitor::handle();
+    EmailClient::handle();
+    delay(10);
+}
